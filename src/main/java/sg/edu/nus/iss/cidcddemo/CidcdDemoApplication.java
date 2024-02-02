a20cb5e991a7ed56cca1aee3e2e3b5fdc1ff76f9
Zer Min Chu <zermin55@gmail.com>
2024-02-02 22:02:16 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/cidcddemo/CidcdDemoApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cidcddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CidcdDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CidcdDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/cidcddemo/CidcdDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cidcddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CidcdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
